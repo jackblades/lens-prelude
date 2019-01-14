9eb64a6675dc51261b41c0f9ba326d8c536423b4
singhpdz <singhpdz@amazon.com>
2019-01-14 20:10:41 +0530

    - added data lenses for creation / manipulation - TODO lenses for hashset, set and intset conflict so only intset is given for now

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Prelude.Galileo
+import Prelude.Containers.Algo
 
 main :: IO ()
 main = undefined
---
+++ src/Prelude/Containers/Algo.hs
@@ -0,0 +1,30 @@
+
+
+
+module Prelude.Containers.Algo (module X) where
+
+
+import Data.Vector.Lens as X
+import Data.IntSet.Lens as X
+
+
+import Data.Array.Lens as X
+import Data.Bits.Lens as X
+import Data.ByteString.Lens as X
+-- import Data.ByteString.Lazy.Lens as X
+-- import Data.ByteString.Strict.Lens as X
+import Data.Complex.Lens as X
+import Data.Data.Lens as X
+import Data.Dynamic.Lens as X
+-- import Data.HashSet.Lens as X hiding (setOf, setmapped)  -- conlicts with Data.IntSet.Lens
+import Data.List.Lens as X
+import Data.Map.Lens as X
+import Data.Sequence.Lens as X hiding (sliced)  -- conlicts with Data.Vector.Lens
+-- import Data.Set.Lens as X hiding (setOf, setmapped)    -- conlicts with Data.IntSet.Lens
+import Data.Text.Lens as X
+-- import Data.Text.Lazy.Lens as X
+-- import Data.Text.Strict.Lens as X
+import Data.Tree.Lens as X
+import Data.Typeable.Lens as X
+-- import Data.Vector.Generic.Lens as X
+
